35dc13b2b8d1562ca5167dc15f52a5e785f00678
reyyuvraj <yuvrajsingh77733999@gmail.com>
2021-10-03 19:05:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/alarmmanager/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.alarmmanager.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.alarmmanager.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
